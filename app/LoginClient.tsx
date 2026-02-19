3e49c9d7b60b6bacdb0369f118c27b474d174006
Vidhilika Gupta <gvidhilika0606@gmail.com>
2026-02-19 11:12:05 +0530

    Refactor authentication flow and implement LoginClient component for Google OAuth to redirect to dashboard if session exists

---
+++ app/LoginClient.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { supabase } from "@/lib/supabaseClient";
+
+export default function LoginClient() {
+  const handleLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+
+    console.log(data, error);
+  };
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <button
+        onClick={handleLogin}
+        className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded-lg text-sm transition"
+      >
+        Login with Google
+      </button>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,17 @@
-"use client";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import LoginClient from "./LoginClient";
 
-import { supabase } from "@/lib/supabaseClient";
+export default async function Home() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-export default function LoginPage() {
-  const handleLogin = async () => {
-    const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: "google",
-      options: {
-        redirectTo: `${location.origin}/auth/callback`,
-      },
-    });
+  if (user) {
+    // If session exists, redirect immediately
+    redirect("/dashboard");
+  }
 
-    console.log(data, error);
-  };
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <button
-        onClick={handleLogin}
-        className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded-lg text-sm transition"
-      >
-        Login with Google
-      </button>
-    </div>
-  );
+  return <LoginClient />;
 }
